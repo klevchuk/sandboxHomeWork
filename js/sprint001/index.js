c6081360880288761809047f1ad20089326aaa8b
v_klevchuk <lemoniklava@gmail.com>
2023-08-28 22:27:14 +0800

    28.08.23 22:27

--- js/sprint001/index.js
+++ js/sprint001/index.js
@@ -234,7 +234,7 @@ console.log(isInRange(33));
 //         }    
 //     }
 //   };
-
+/* rjvvtyn */
 const rps = (p1, p2) => { 
     if ((p1 == "scissors" && p2 == "paper") || (p1 == "paper" && p2 == "rock") || (p1 == "rock" && p2 == "scissors")) {
       return "Player 1 won!";
